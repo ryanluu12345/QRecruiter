0e028db8f1cf79e41060601583736a6470fe2bbf
VictorLe <vle7@uci.edu>
2019-01-12 02:29:55 -0800

    basic scope 4 all & html/css for login & signup

--- client/src/app.js
+++ client/src/app.js
@@ -22,17 +22,56 @@ app
         return $http.get(users);
       }
 
+      
+
       /* Put the name of the functions on the left and define the functions on the right */
 
     };
   }
 ]);    
     
-/* Controller for the login page */
-app.controller('LoginCtrl', ['$scope', 'UserService', function($scope, UserService){
-  $scope.user = null;
+
+/* Controller for the recruiter page */
+app.controller('RecruiterCtrl', ['$scope', 'UserService', function($scope, UserService){
+  $scope.location = null;
+  $scope.position = null;
+  UserService.getUser()
+    .success(function(data) {
+      $scope.user = "y";
+    });
+}]);    
+
+/* Controller for the user page */
+app.controller('UserCtrl', ['$scope', 'UserService', function($scope, UserService){
+  $scope.username = null;
   UserService.getUser()
     .success(function(data) {
       $scope.user = data;
     });
 }]);    
+
+/* Controller for the signup page */
+app.controller('SignUpCtrl', ['$scope', 'UserService', function($scope, UserService){
+  $scope.username = null;
+  $scope.pass = null;
+  $scope.eMail = null;
+  $scope.phone = null;
+  $scope.linkedin = null;
+  UserService.getUser()
+    .success(function(data) {
+      $scope.user = data;
+    });
+}]);    
+
+/* Controller for the login page */
+app.controller('LoginCtrl', ['$scope', 'UserService', function($scope, UserService){
+  $scope.username = null;
+  $scope.pasword = null; 
+  UserService.getUser()
+    .success(function(data) {
+      $scope.username = data;
+    });
+}]);    
+
+
+
--- client/src/routes.js
+++ client/src/routes.js
@@ -10,13 +10,13 @@ angular.module('reg')
       const recruiterState = {
         name: 'recruiter',
         url: '/recruiter',
-        template: '<h3>You are on the recruiter page!</h3>'
+        templateUrl: 'views/recruiter/recruiter.html'
       }
 
       const userState = {
         name: 'user',
         url: '/user',
-        template: '<h3>You are on the user page</h3>'
+        templateUrl: 'views/user/user.html'
       }
 
       const signupState = {
